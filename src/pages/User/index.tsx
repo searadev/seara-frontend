f1b91089b7348f9ec35495d614ef7e91bfcb79e3
searadev <searadejesusdev@gmail.com>
2022-09-21 16:15:19 -0300

    add pages

---
+++ src/pages/User/index.tsx
@@ -0,0 +1,15 @@
+const User = () => {
+    return (
+      <>
+        <div className="user-container">
+          <div className="user-card">
+            <div className="user-content-container">
+              <h1>Usuários</h1>;
+            </div>
+          </div>          
+        </div>            
+      </>
+    );
+  };
+  
+  export default User;
