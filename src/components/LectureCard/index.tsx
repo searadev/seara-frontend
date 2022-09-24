eb811f53509a839d997db4837c68a9072dbfd287
searadev <searadejesusdev@gmail.com>
2022-09-23 22:13:47 -0300

    add psychography, class and lecture pages and pages card

---
+++ src/components/LectureCard/index.tsx
@@ -0,0 +1,17 @@
+import './styles.css';
+
+const LectureCard = () => {
+
+    return (
+        <div className="base-card lecture-card">
+            <div className="lecture-top-container">
+                <p>15_08_22 Rita Navarro</p>
+            </div>
+            <div className="lecture-body-container">
+                <p>2022-08-15</p>
+            </div>            
+        </div>
+    );
+}
+
+export default LectureCard;
--- src/pages/Message/index.tsx
+++ src/pages/Message/index.tsx
@@ -1,3 +1,4 @@
+import MessageCard from "components/MessageCard";
 import Navbar from "components/Navbar";
 import './styles.css';
 
@@ -5,12 +6,22 @@ const Message = () => {
     return (
       <>
         <Navbar />
-        <div className="message-container">
-          <div className="message-card">
-            <div className="message-content-container">
-              <h1>Hello Seara</h1>
-            </div>
-          </div>          
+        <div className="container my-4 message">
+          <div className="row">
+            <MessageCard />
+          </div>
+          <div className="row">
+            <MessageCard />
+          </div>
+          <div className="row">
+            <MessageCard />
+          </div>
+          <div className="row">
+            <MessageCard />
+          </div>
+          <div className="row">
+            <MessageCard />
+          </div>
         </div> 
       </>
     );
