31c855c1a5a2f2aaa75656360f2048bfe13d4304
searadev <searadejesusdev@gmail.com>
2022-09-25 15:21:27 -0300

    add class, psychography, lecture and message details

--- src/pages/ClassRoomDetails/index.tsx
+++ src/pages/ClassRoomDetails/index.tsx
@@ -1,8 +1,28 @@
 import './styles.css';
+import { ReactComponent as ArrowIcon } from 'assets/images/arrow.svg';
 
 const ClassRoomDetails = () => {
     return (
-        <h1>Class Room Details</h1>
+        <div className="classroom-details-container">
+            <div className="base-card classroom-details-card">
+                <div className="goback-container">
+                    <ArrowIcon />
+                    <h2>VOLTAR</h2>
+                </div>
+                <div className="classroom-content-container">
+                    <div className="classroom-text-container">
+                    <iframe width="1050" height="720" src="https://www.youtube.com/embed/WsEv01p3GXU" title="Como inserir vídeo do YouTube no seu site HTML (Embed)" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" ></iframe>
+                    </div>
+                    <div>
+                        <div className="classroom-data-container">
+                            <h1>11/11/11</h1>
+                            <h1>-</h1>
+                            <h1>24ª Aula – Jesus; Anunciação, Nascimento, Família, Vida Dos 12 Aos 30</h1>
+                        </div>                    
+                    </div>
+                </div>                
+            </div>            
+        </div>
     );
 }
 
--- src/pages/LectureDetails/index.tsx
+++ src/pages/LectureDetails/index.tsx
@@ -1,8 +1,28 @@
 import './styles.css';
+import { ReactComponent as ArrowIcon } from 'assets/images/arrow.svg';
 
 const LectureDetails = () => {
     return (
-        <h1>Lecture Details</h1>
+        <div className="lecture-details-container">
+            <div className="base-card lecture-details-card">
+                <div className="goback-container">
+                    <ArrowIcon />
+                    <h2>VOLTAR</h2>
+                </div>
+                <div className="lecture-content-container">
+                    <div className="lecture-text-container">
+                    <iframe width="1050" height="720" src="https://www.youtube.com/embed/WsEv01p3GXU" title="Como inserir vídeo do YouTube no seu site HTML (Embed)" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" ></iframe>
+                    </div>
+                    <div>
+                        <div className="lecture-data-container">
+                            <h1>11/11/11</h1>
+                            <h1>-</h1>
+                            <h1>24ª Aula – Jesus; Anunciação, Nascimento, Família, Vida Dos 12 Aos 30</h1>
+                        </div>                    
+                    </div>
+                </div>                
+            </div>            
+        </div>
     );
 }
 
