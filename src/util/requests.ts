912640ea0d8695de0afd03d8b3b973eb28656259
searadev <searadejesusdev@gmail.com>
2022-09-28 15:54:56 -0300

    adjust api url

--- src/util/requests.ts
+++ src/util/requests.ts
@@ -1,2 +1,2 @@
 /*export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://192.168.5.242:8080'*/
-export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'https://searafront.herokuapp.com:8080'
+export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'https://searadev.herokuapp.com'
