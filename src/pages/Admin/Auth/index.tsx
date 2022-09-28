6c68d65a482fc603a4c77eb82b6e631dd9f8dec7
searadev <searadejesusdev@gmail.com>
2022-09-28 18:08:06 -0300

    Auth and rotes

---
+++ src/pages/Admin/Auth/index.tsx
@@ -0,0 +1,29 @@
+import { ReactComponent as AuthImage} from 'assets/images/auth-image.svg';
+import { Route, Switch } from 'react-router-dom';
+
+const Auth = () => {
+    return (
+        <div className="auth-container">
+            <div className="auth-banner-container">
+                <h1>Faça os cadastros</h1>
+                <AuthImage />
+            </div>
+            <div className="auth-form-container">
+                <Switch>
+                    <Route path="/admin/auth/login">
+                        <h1>Card de Login</h1>
+                    </Route>
+                    <Route path="/admin/auth/signup">
+                        <h1>Card de Signup</h1>
+                    </Route>
+                    <Route path="/admin/auth/recover">
+                        <h1>Card de Recover</h1>
+                    </Route>
+                </Switch>
+
+            </div>
+        </div>
+    );
+}
+
+export default Auth;
