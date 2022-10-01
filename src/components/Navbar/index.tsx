b800d40572431390a0be370d93a822dbb8cf4051
searadev <searadejesusdev@gmail.com>
2022-10-01 06:47:31 -0300

    Login/Logout logic

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,8 +1,40 @@
 import './styles.css';
 import 'bootstrap/js/src/collapse.js';
 import { Link, NavLink } from 'react-router-dom';
+import { getTokenData, isAuthenticated, removeAuthData, TokenData } from 'util/requests';
+import { useEffect, useState } from 'react';
+import history from 'util/history';
+
+type AuthData = {
+  authenticated: boolean;
+  tokenData?: TokenData;
+};
 
 const Navbar = () => {
+  const [authData, setAuthData] = useState<AuthData>({ authenticated: false });
+
+  useEffect(() => {
+    if (isAuthenticated()) {
+      setAuthData({
+        authenticated: true,
+        tokenData: getTokenData(),
+      });
+    } else {
+      setAuthData({
+        authenticated: false,
+      });
+    }
+  }, []);
+
+  const handleLogoutClick = (event: React.MouseEvent<HTMLAnchorElement>) => {
+    event.preventDefault();
+    removeAuthData();
+    setAuthData({
+      authenticated: false,
+    });
+    history.replace('/');
+  }
+
   return (
     <nav className="navbar navbar-expand-lg navbar-dark main-nav">
       <div className="container-fluid">
@@ -24,21 +56,39 @@ const Navbar = () => {
         <div className="collapse navbar-collapse" id="searadev-navbar">
           <ul className="navbar-nav offset-md-2 main-menu">
             <li>
-              <NavLink to="/message" activeClassName="active" exact>MENSAGENS MOTIVACIONAIS</NavLink>
+              <NavLink to="/message" activeClassName="active" exact>
+                MENSAGENS MOTIVACIONAIS
+              </NavLink>
             </li>
             <li>
-              <NavLink to="/psychography" activeClassName="active">PSICOGRAFIAS</NavLink>
+              <NavLink to="/psychography" activeClassName="active">
+                PSICOGRAFIAS
+              </NavLink>
             </li>
             <li>
-              <NavLink to="/lecture" activeClassName="active">PALESTRAS</NavLink>
+              <NavLink to="/lecture" activeClassName="active">
+                PALESTRAS
+              </NavLink>
             </li>
             <li>
-              <NavLink to="/classroom" activeClassName="active">AULAS</NavLink>
+              <NavLink to="/classroom" activeClassName="active">
+                AULAS
+              </NavLink>
             </li>
             <li>
-              <NavLink to="/admin" activeClassName="active">ADMIN</NavLink>
+              <NavLink to="/admin" activeClassName="active">
+                ADMIN
+              </NavLink>
             </li>
           </ul>
+        </div>
+
+        <div>
+          {authData.authenticated ? (
+            <a href='#logout' onClick={handleLogoutClick}>LOGOUT</a>
+          ) : (
+            <Link to='/admin/auth'>LOGIN</Link>
+          )}
         </div>
       </div>
     </nav>
