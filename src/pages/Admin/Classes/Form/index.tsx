c3b0cf5bfcf4624c5bd3280f82e1fa36c2ccd167
searadev <searadejesusdev@gmail.com>
2022-10-09 13:42:44 -0300

    Form buttons styling

--- src/pages/Admin/Classes/Form/index.tsx
+++ src/pages/Admin/Classes/Form/index.tsx
@@ -6,19 +6,34 @@ const Form = () => {
       <div className="base-card class-crud-form-card">
         <h1 className="class-crud-form-title">DADOS DA AULA</h1>
         <form action="">
-          <div className="row">
-            <div className="col-lg-6">
-              <input type="text" className='form-control base-input' />
-              <input type="text" className='form-control base-input' />
-              <input type="text" className='form-control base-input' />
+          <div className="row class-crud-inputs-container">
+            <div className="col-lg-6 class-crud-inputs-left-container">
+              <div className="margin-botton-30">
+                <input type="text" className="form-control base-input" />
+              </div>
+              <div className="margin-botton-30">
+                <input type="text" className="form-control base-input" />
+              </div>
+              <div>
+                <input type="text" className="form-control base-input" />
+              </div>
             </div>
             <div className="col-lg-6">
-              <textarea rows={10} className='form-control base-input'></textarea>
+              <div>
+                <textarea
+                  rows={10}
+                  className="form-control base-input h-auto"
+                ></textarea>
+              </div>
             </div>
           </div>
-          <div>
-            <button className='btn btn-outline-danger'>CANCELAR</button>
-            <button className='btn btn-primary'>SALVAR</button>
+          <div className="xpto-agora-vai">
+            <button className="btn btn-outline-danger xpto-button">
+              CANCELAR
+            </button>
+            <button className="btn btn-primary xpto-button text-white">
+              SALVAR
+            </button>
           </div>
         </form>
       </div>
