8fb69917f8560402fca0e291ba8db2ff7aa59716
searadev <searadejesusdev@gmail.com>
2022-10-16 08:18:31 -0300

    Message admin crud forms

--- src/pages/Admin/Messages/Form/index.tsx
+++ src/pages/Admin/Messages/Form/index.tsx
@@ -1,7 +1,186 @@
+import { AxiosRequestConfig } from 'axios';
+import { useEffect, useState } from 'react';
+import { useForm, Controller } from 'react-hook-form';
+import { useHistory, useParams } from 'react-router-dom';
+import Select from 'react-select';
+import { Medium } from 'types/medium';
+import { Message } from 'types/message';
+import { BASE_URL, requestBackend } from 'util/requests';
 import './styles.css';
 
+type UrlParams = {
+  messageId: string;
+};
+
 const Form = () => {
-  return <h1>Tela Form</h1>;
+  const { messageId } = useParams<UrlParams>();
+  const isEditing = messageId !== 'create';
+  const history = useHistory();
+
+  const [selectMediuns, setSelectMediuns] = useState<Medium[]>([]);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setValue,
+    control,
+  } = useForm<Message>();
+
+  useEffect(() => {
+    requestBackend({ url: '/mediuns', withCredentials: true }).then(
+      (response) => {
+        setSelectMediuns(response.data.content);
+      }
+    );
+  }, []);
+
+  useEffect(() => {
+    console.log(messageId);
+    if (isEditing) {
+      requestBackend({ url: `/messages/${messageId}` }).then((response) => {
+        const message = response.data as Message;
+        setValue('title', message.title);
+        setValue('text', message.text);
+        setValue('date', message.date);
+        setValue('fullName', message.fullName);
+        setValue('medium', message.medium);
+      });
+    }
+  }, [isEditing, messageId, setValue]);
+  const onSubmit = (formData: Message) => {
+    const data = {
+      ...formData,
+      medium: isEditing ? formData.medium : { id: 1 },
+    };
+    const config: AxiosRequestConfig = {
+      method: isEditing ? 'PUT' : 'POST',
+      url: isEditing ? `/messages/${messageId}` : '/messages',
+      baseURL: BASE_URL,
+      data: data,
+      withCredentials: true,
+    };
+
+    requestBackend(config).then(() => {
+      history.push('/admin/messages');
+    });
+  };
+
+  const handleCancel = () => {
+    history.push('/admin/messages');
+  };
+
+  return (
+    <div className="message-crud-content">
+      <div className="base-card message-crud-form-card">
+        <h1 className="message-crud-form-title">DADOS DA MENSAGEM</h1>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="row message-crud-inputs-container">
+            <div className="col-lg-6 message-crud-inputs-left-container">
+              <div className="margin-botton-30">
+                <input
+                  {...register('title', {
+                    required: 'Campo obrigatório',
+                  })}
+                  type="text"
+                  className={`form-control base-input ${
+                    errors.title ? 'is-invalid' : ''
+                  }`}
+                  placeholder="Título"
+                  name="title"
+                />
+                <div className="invalid-feedback d-block">
+                  {errors.title?.message}
+                </div>
+              </div>
+              <div className="margin-botton-30">
+                <input
+                  {...register('fullName', {
+                    required: 'Campo obrigatório',
+                  })}
+                  type="text"
+                  className={`form-control base-input ${
+                    errors.title ? 'is-invalid' : ''
+                  }`}
+                  placeholder="Nome"
+                  name="fullName"
+                />
+                <div className="invalid-feedback d-block">
+                  {errors.fullName?.message}
+                </div>
+              </div>
+              <div className="margin-botton-30">
+                <input
+                  {...register('date', {
+                    required: 'Campo obrigatório',
+                  })}
+                  type="text"
+                  className={`form-control base-input ${
+                    errors.date ? 'is-invalid' : ''
+                  }`}
+                  placeholder="Data"
+                  name="date"
+                />
+                <div className="invalid-feedback d-block">
+                  {errors.date?.message}
+                </div>
+              </div>
+              <div className="margin-botton-30">
+                <Controller
+                  name="medium"
+                  rules={{ required: true }}
+                  control={control}
+                  render={({ field }) => (
+                    <Select
+                      {...field}
+                      options={selectMediuns}
+                      classNamePrefix="message-crud-select"
+                      getOptionLabel={(medium: Medium) => medium.fullName}
+                      getOptionValue={(medium: Medium) => String(medium.id)}
+                    />
+                  )}
+                />
+                {errors.medium && (
+                  <div className="invalid-feedback d-block">
+                    Campo obrigatório
+                  </div>
+                )}
+              </div>
+            </div>
+            <div className="col-lg-6">
+              <div className="margin-botton-30">
+                <textarea
+                  rows={10}
+                  {...register('text', {
+                    required: 'Campo obrigatório',
+                  })}
+                  className={`form-control base-input h-auto ${
+                    errors.text ? 'is-invalid' : ''
+                  }`}
+                  placeholder="Texto"
+                  name="text"
+                />
+                <div className="invalid-feedback d-block">
+                  {errors.text?.message}
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="xpto-agora-vai">
+            <button
+              className="btn btn-outline-danger xpto-button"
+              onClick={handleCancel}
+            >
+              CANCELAR
+            </button>
+            <button className="btn btn-primary xpto-button text-white">
+              SALVAR
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Form;
--- src/pages/Admin/Messages/List/index.tsx
+++ src/pages/Admin/Messages/List/index.tsx
@@ -1,7 +1,54 @@
+import axios, { AxiosRequestConfig } from 'axios';
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Message } from 'types/message';
+import { SpringPage } from 'types/vendor/spring';
+import { BASE_URL } from 'util/requests';
+import MessageCrudCard from '../MessageCrudCard';
 import './styles.css';
 
 const List = () => {
-  return <h1>Tela List</h1>;
+  const [page, setPage] = useState<SpringPage<Message>>();
+
+  useEffect(() => {
+    getmessages();
+  }, []);
+
+  const getmessages = () => {
+    const config: AxiosRequestConfig = {
+      method: 'GET',
+      url: '/messages',
+      baseURL: BASE_URL,
+      params: {
+        page: 0,
+        size: 12,
+      },
+    };
+
+    axios(config).then((response) => {
+      setPage(response.data);
+    });
+  };
+
+  return (
+    <div className="message-crud-content">
+      <div className="message-crud-bar-container">
+        <Link to="/admin/messages/create">
+          <button className="btn btn-primary text-white btn-crud-add">
+            ADICIONAR
+          </button>
+        </Link>
+        <div className="base-card message-filter-container">Search Bar</div>
+      </div>
+      <div className="row">
+        {page?.content.map((palestras) => (
+          <div className="col-sm-6 col-md-12" key={palestras.id}>
+            <MessageCrudCard message={palestras} onDelete={() => getmessages()} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default List;
