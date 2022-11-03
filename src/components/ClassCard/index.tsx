dbc0bffb6c637c7a648cbdfbbc609099af6853b5
searadev <searadejesusdev@gmail.com>
2022-11-02 22:37:21 -0300

    add iframe classcard and lecturecard

--- src/components/ClassCard/index.tsx
+++ src/components/ClassCard/index.tsx
@@ -2,22 +2,26 @@ import { ClassRoom } from 'types/classroom';
 import './styles.css';
 
 type Props = {
-    classRoom: ClassRoom;
-}
+  classRoom: ClassRoom;
+};
 
-const ClassCard = ({ classRoom } : Props) => {
-
-    return (
-        <div className="base-card class-card">
-            <div className="class-top-container">
-                <p>{classRoom.title}</p>
-            </div>
-            <div className="class-container">
-                <p>{classRoom.date}</p>
-                <span>{classRoom.module.name}</span>
-            </div>
+const ClassCard = ({ classRoom }: Props) => {
+  return (
+    <div className="base-card class-card">
+      <div className="ratio ratio-16x9 class-videooo-container">
+        <iframe src={classRoom?.url} title={classRoom?.title}></iframe>
+      </div>
+      <div>
+        <div className="class-top-container">
+          <p>{classRoom.title}</p>
         </div>
-    );
-}
+        <div className="class-container">
+          <p>{classRoom.date}</p>
+          <span>{classRoom.module.name}</span>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default ClassCard;
--- src/components/LectureCard/index.tsx
+++ src/components/LectureCard/index.tsx
@@ -2,21 +2,25 @@ import { Lecture } from 'types/lecture';
 import './styles.css';
 
 type Props = {
-    lecture: Lecture;
-}
+  lecture: Lecture;
+};
 
-const LectureCard = ({ lecture } : Props) => {
-
-    return (
-        <div className="base-card lecture-card">
-            <div className="lecture-top-container">
-                <p>{lecture.title}</p>
-            </div>
-            <div className="lecture-body-container">
-                <p>{lecture.date}</p>
-            </div>            
+const LectureCard = ({ lecture }: Props) => {
+  return (
+    <div className="base-card lecture-card">
+      <div className="ratio ratio-16x9 lecture-videooo-container">
+        <iframe src={lecture?.url} title={lecture?.title}></iframe>
+      </div>
+      <div>
+        <div className="lecture-top-container">
+          <p>{lecture.title}</p>
         </div>
-    );
-}
+        <div className="lecture-body-container">
+          <p>{lecture.date}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default LectureCard;
