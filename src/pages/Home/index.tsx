ef9992b910802f6518794a29b099a11ce05b635f
searadev <searadejesusdev@gmail.com>
2023-05-28 12:29:55 -0300

    add home page

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,38 @@
+import './styles.css';
+
+const Home = () => {
+
+    return (
+        <>
+    <h1>
+        ASSOCIAÇÃO ESPÍRITA SEARA DE JESUS
+    </h1>
+    <h4>
+        Cel.: (11)9.9691-3012
+    </h4>
+    <h4>
+        Horário de funcionamento - 2023
+    </h4>
+    <h4>
+        Quarta-feira: das 19h às 20h30.
+    </h4>
+    <h4>
+        Sábado: das 19h às 20h30.
+    </h4>
+    <h4>
+        Palestra pública
+    </h4>
+    <h4>
+        Tratamento médico-espiritual
+    </h4>
+    <h4>
+        Rua Júpiter, 206 - jd. Novo Embu - Embu das Artes - SP
+    </h4>
+        </>
+    
+    
+    );   
+
+};
+
+export default Home;
