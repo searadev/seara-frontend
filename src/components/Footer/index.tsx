c2a819002b9d7d5e5dd5db589f7e86777f42ed7f
searadev <searadejesusdev@gmail.com>
2024-07-11 23:08:36 -0300

    Adjust classroom layout

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -6,25 +6,32 @@ import './styles.css';
 const Footer = () => {
   return (
     <>
-      <footer className="text-center bg-white ">               
-        <div className="text-center p-4">          
-        <a
-              href="https://www.youtube.com/channel/UCIessbLUBPCA9106K18V43w"
-              className="youtube social mx-2"
-            >
-              <FontAwesomeIcon icon={faYoutube} color="#eb3223" size="2x"  />
-            </a>
-            <a
-              href="https://pt-br.facebook.com/searadejesus/"
-              className="facebook social mx-2"
-            >
-              <FontAwesomeIcon icon={faFacebook} color="#4968ad" size="2x"  />
-            </a>
+      <footer className="text-center bg-white ">
+        <div className="text-center p-3">
+          <a
+            href="https://www.youtube.com/channel/UCIessbLUBPCA9106K18V43w"
+            className="youtube social mx-2"
+          >
+            <FontAwesomeIcon icon={faYoutube} color="#eb3223" size="2x" />
+          </a>
+          <a
+            href="https://pt-br.facebook.com/searadejesus/"
+            className="facebook social mx-2"
+          >
+            <FontAwesomeIcon icon={faFacebook} color="#4968ad" size="2x" />
+          </a>
         </div>
-        <div className="text-center p-4">
+        <div className="text-center p-3">
           <div className="endereco-container">
-          Rua Júpiter, 206 - jd. Novo Embu - Embu das Artes - SP
-          </div>            
+            Rua Júpiter, 206 - jd. Novo Embu - Embu das Artes - SP
+          </div>
+          <div className="text-center p-3">
+
+          </div>
+
+          <div className="text-center p-3">
+
+          </div>
         </div>
       </footer>
     </>
