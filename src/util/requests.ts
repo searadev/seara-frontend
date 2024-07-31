8eee262c2575d43cf1c30dcfe6f9846a1fc67631
searadev <searadejesusdev@gmail.com>
2024-07-31 02:13:33 -0300

    Add dotenv

--- src/util/requests.ts
+++ src/util/requests.ts
@@ -3,11 +3,10 @@ import qs from 'qs';
 import history from './history';
 import { getAuthData } from './storage';
 
-const dotenv = require('dotenv').config();
 
-export const BASE_URL = process.env.BACKEND_URL;
-const CLIENT_ID = process.env.CLIENT_ID;
-const CLIENT_SECRET = process.env.CLIENT_SECRET;
+export const BASE_URL = process.env.REACT_APP_BASE_URL;
+const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
+const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET;
 
 type LoginData = {
   username: string;
